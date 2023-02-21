2ffc320e48dab01b3c7eea81c7821931962e9cdf
Doheera-kosi <kupourevans99@gmail.com>
2023-02-21 01:44:35 +0000

    Modify my certifications

--- src/components/certification/Certifications.jsx
+++ src/components/certification/Certifications.jsx
@@ -1,175 +1,173 @@
-import React from 'react'
-import {FaAward} from 'react-icons/fa'
+import React, { useState } from 'react';
+import { FaAward } from 'react-icons/fa';
 import './certifications.css'
 
-const Certifications = () => {
+function Certificates() {
+  const [showAll, setShowAll] = useState(false);
+
+  const certificates = [
+    {
+      id: 1,
+      issuer: 'IBM',
+      title: 'Introduction to Hardware and Operating Systems',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/VJ6JPRSZVYRN',
+    },
+    {
+      id: 2,
+      issuer: 'IBM',
+      title: 'Introduction to Software, Programming, and Databases',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/A48AEFNQE6M6',
+    },
+    {
+      id: 3,
+      issuer: 'IBM',
+      title: 'Introduction to Cloud Computing',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/ZYD8HLBKNUJA',
+    },
+    {
+      id: 4,
+      issuer: 'IBM',
+      title: 'Technical Support Case Studies and Capstone',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/KDT2J8M6AZQ4',
+    },
+    {
+      id: 5,
+      issuer: 'IBM',
+      title: 'Introduction to Cybersecurity Essentials',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/MERCNA2LLMLM',
+    },
+    {
+      id: 6,
+      issuer: 'IBM',
+      title: 'Introduction to Networking and Storage',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/6S9H25X6SX8S',
+    },
+    {
+      id: 7,
+      issuer: 'IBM',
+      title: 'Introduction to Technical Support',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/SDC9CVBWEKFY',
+    },
+    {
+      id: 8,
+      issuer: 'Meta',
+      title: 'Introduction to Databases for Back-End Development',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/LPL4Q2KDY6KE',
+    },
+    {
+      id: 9,
+      issuer: 'Meta',
+      title: 'Introduction to Back-End Development',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/MAT6EWP8REUD',
+    },
+    {
+      id: 10,
+      issuer: 'Meta',
+      title: 'Programming in Python',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/ML4C7DW3A9P6',
+    },
+    {
+      id: 11,
+      issuer: 'IBM',
+      title: 'Data Analysis with Python',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/5YD4Z6CUCHGH',
+    },
+    {
+      id: 12,
+      issuer: 'IBM',
+      title: 'Applied Data Science Capstone',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/PEE6TF54HMBF',
+    },
+    {
+      id: 13,
+      issuer: 'IBM',
+      title: 'Databases and SQL for Data Science with Python',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/NUGWXEFLSQ2R',
+    },
+    {
+      id: 14,
+      issuer: 'IBM',
+      title: 'Python Project for Data Science',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/J2AE7W83SAY6',
+    },
+    {
+      id: 15,
+      issuer: 'IBM',
+      title: 'Machine Learning with Python',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/9SKLKWZXVSX6',
+    },
+    {
+      id: 16,
+      issuer: 'Interactive Brokers',
+      title: 'Forex - Trading Around the World',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/5MABWL4S8UUW',
+    },
+    {
+      id: 17,
+      issuer: 'Yale',
+      title: 'Finacial Markets',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/FP8VL8F4UPER',
+    },
+    {
+      id: 18,
+      issuer: 'IBM',
+      title: 'Tools for Data Science',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/YJHWR3532H43',
+    },
+    {
+      id: 19,
+      issuer: 'IBM',
+      title: 'Data Science Methodolog',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/TTSC4TTNW8Z9',
+    },
+    {
+      id: 20,
+      issuer: 'IBM',
+      title: 'What is Data Science',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/X4DFVUMPK8BB',
+    },
+    {
+      id: 21,
+      issuer: 'IBM',
+      title: 'Python for Data Science, AI & Development',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/8ZG2VUL7XP8S',
+    },
+    {
+      id: 22,
+      issuer: 'IBM',
+      title: 'Data Visualization with Python',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/PEE6TF54HMBF',
+    },
+    // Add the remaining certificates here
+  ];
+
+  const visibleCertificates = showAll ? certificates : certificates.slice(0, 5);
+
   return (
-    <section id="certificates">
-      <h5>Awards</h5>
-      <h2>My Certifications</h2>
+    <>
+      <section id="certificates">
+        <h5>Awards</h5>
+        <h2>My Certifications</h2>
 
-      <div className="container certificate__container">
-        <div className="certificates__options">
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Hardware and Operating Systems</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/VJ6JPRSZVYRN" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Software, Programming, and Databases</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/A48AEFNQE6M6" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Cloud Computing</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/ZYD8HLBKNUJA" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Technical Support Case Studies and Capstone</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/KDT2J8M6AZQ4" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Cybersecurity Essentials</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/MERCNA2LLMLM" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Networking and Storage</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/6S9H25X6SX8S" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Introduction to Technical Support</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/SDC9CVBWEKFY" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>Meta</h4>
-            <h5>Introduction to Databases for Back-End Development</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/LPL4Q2KDY6KE" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>Meta</h4>
-            <h5>Programming in Python</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/ML4C7DW3A9P6" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Data Analysis with Python</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/5YD4Z6CUCHGH" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Applied Data Science Capstone</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/KMDF5B7D5QUK" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Data Visualization with Python</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/PEE6TF54HMBF" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Databases and SQL for Data Science with Python</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/NUGWXEFLSQ2R" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Python Project for Data Science</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/J2AE7W83SAY6" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>Meta</h4>
-            <h5>Introduction to Back-End Development</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/MAT6EWP8REUD" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Machine Learning with Python</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/9SKLKWZXVSX6" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>Interactive Brokers</h4>
-            <h5>Forex - Trading Around the World</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/5MABWL4S8UUW" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>Yale</h4>
-            <h5>Financial Markets</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/FP8VL8F4UPER" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Tools for Data Science</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/YJHWR3532H43" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-          
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Data Science Methodology</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/TTSC4TTNW8Z9" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
+        <div className="container certificate__container">
 
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>What is Data Science</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/X4DFVUMPK8BB" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-
-          <article className="certificate__option">
-            <FaAward className='certificate__option-icon' />
-            <h4>IBM</h4>
-            <h5>Python for Data Science, AI & Development</h5>
-            <a href="https://www.coursera.org/account/accomplishments/certificate/8ZG2VUL7XP8S" target='_blank' rel='noreferrer'>Click to view</a>
-          </article>
-
+          <div className="certificates__options">
+            {visibleCertificates.map((certificate, index) => (
+              <article className="certificate__option" key={index}>
+                <FaAward className='certificate__option-icon' />
+                <h4>{certificate.issuer}</h4>
+                <h5>{certificate.title}</h5>
+                <a href={certificate.link} target='_blank' rel='noreferrer'>Click to view</a>
+              </article>
+            ))}
+            {!showAll && (
+              <button onClick={() => setShowAll(true)}>Show More</button>
+            )}
+          </div>
         </div>
-        {/* End of Cantact options */}
-      </div>
-    </section>
-  )
+      </section>
+    </>
+  );
 }
 
-export default Certifications
+export default Certificates;
