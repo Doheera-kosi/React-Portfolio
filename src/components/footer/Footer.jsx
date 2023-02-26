fd292efcd2968229f83001f435f2a869e2fe1749
Doheera-kosi <kupourevans99@gmail.com>
2023-02-26 13:47:42 +0000

    Update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,10 +5,10 @@ import {FaFacebookF, FaLinkedin, FaAngellist, FaTwitter, FaWhatsapp} from 'react
 const Footer = () => {
   return (
     <footer id="footer">
-      <a href="home" className='footer__logo'>Evans Kupour</a>
+      <a href="#home" className='footer__logo'>Evans Kupour</a>
 
       <ul className="permalinks">
-        <li><a href="home">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,11 +9,11 @@ import { useState } from 'react'
 
 const Nav = () => {
 
-  const [activeNav, setActiveNav] = useState('home')
+  const [activeNav, setActiveNav] = useState('#home')
 
   return (
     <nav>
-      <a href="home" onClick={() => setActiveNav('home')} className={activeNav === 'home' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServerLine /></a>
