06ce2af7fc87b0f95bd1c2d820016e11eac739aa
Evans Kupour <kupourevans99@gmail.com>
2023-04-15 20:42:03 +0000

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CV from '../../assets/Evans_Kupour_Resume.pdf'
+// import CV from '../../assets/Evans_Kupour_Resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      {/* <a href={CV} download className='btn'>Download CV</a> */}
+      <a href="https://docs.google.com/document/d/1q1_GE2zFmGiUGwJ8oVGmtWGXBYsYFBUzL7JvB1BFR8U/edit?usp=sharing" target='_blank' rel='noreferrer' download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
   )
