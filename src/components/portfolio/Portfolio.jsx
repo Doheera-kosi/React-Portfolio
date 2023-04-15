2ae57f48065d97c7fc09e94d6c30821faebef898
Evans Kupour <kupourevans99@gmail.com>
2023-04-15 20:12:27 +0000

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,6 +12,7 @@ const data = [
     id: 1,
     image: Port1,
     title: 'Activity Manager (Todo-list)',
+    desc: 'Task manager app for productivity and organization.',
     github: 'https://github.com/Doheera-kosi/todo-list-app',
     demo: 'https://doheera-kosi.github.io/todo-list-app/dist/'
   },
@@ -19,20 +20,23 @@ const data = [
     id: 2,
     image: Port2,
     title: 'Food Palace - (Non-mobile)',
+    desc: 'Web app for displaying menus/dishes',
     github: 'https://github.com/Doheera-kosi/Food-Palace',
     demo: 'https://doheera-kosi.github.io/Food-Palace/dist/'
   },
   {
     id: 3,
     image: Port3,
-    title: 'Expenses Manager - (Mobile)',
-    github: 'https://github.com/Doheera-kosi/Budget-App/tree/development',
-    demo: 'https://budgets-ek.onrender.com/'
+    title: 'Travel Website',
+    desc: 'Simple and responsive travel website built with HTML, CSS , and JavaScript',
+    github: 'https://github.com/Doheera-kosi/Travel-Site',
+    demo: 'https://evans-travel-site.netlify.app/'
   },
   {
     id: 4,
     image: Port4,
     title: 'Crypto Stats Dashboard - (Mobile)',
+    desc: 'Displays statistics for list of crypto coins',
     github: 'https://github.com/Doheera-kosi/Crypto-Metrics-App',
     demo: 'https://beautiful-cobbler-2a0f6f.netlify.app/'
   },
@@ -40,6 +44,7 @@ const data = [
     id: 5,
     image: Port5,
     title: 'JavaScript Currency Converter',
+    desc: 'Web app for converting currencies instantly with ease.',
     github: 'https://github.com/Doheera-kosi/JavaScript-Currency-Converter/tree/working',
     demo: 'https://doheera-kosi.github.io/JavaScript-Currency-Converter/'
   },
@@ -47,6 +52,7 @@ const data = [
     id: 6,
     image: Port6,
     title: 'Leaderboard-(Non-mobile)',
+    desc: 'Leaderboard app is for showing scores',
     github: 'https://github.com/Doheera-kosi/Leaderboard-setup-project/tree/Leaderboard-final-tuches',
     demo: 'https://doheera-kosi.github.io/Leaderboard-setup-project/dist/'
   },
@@ -61,13 +67,14 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, desc, github, demo}) => {
             return (
               <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+              <h5 className='desc'>{desc}</h5>
               <div className="portfolio__item-cta">
                 <a href={github} className='btn'>Github</a>
                 <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
